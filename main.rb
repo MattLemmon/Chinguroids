faf1826edd7b317c6543d5b5f3f5d72af26df3f6
Freddan962 <fredde.sander@gmail.com>
2013-08-11 23:45:30 +0200

    commit

--- main.rb
+++ main.rb
@@ -1,26 +1,55 @@
 require 'Chingu'
 
 class Game < Chingu::Window
-	attr_reader :WIDTH, :HEIGHT
+	attr_reader :WIDTH, :HEIGHT, :NAME
 
 	def initialize
-		@WIDTH, @HEIGHT = 800, 600
+		# Window START
+		@WIDTH, @HEIGHT, @NAME = 800, 600, "ChinguRoids 0.01 - Fractional"
 		super(800, 600, false)
+		self.caption = @NAME
+		# Window END
 
 		self.input = { :escape => :exit}
 
-		push_game_state(Play)
+		push_game_state(Introduction)
 	end
 end
 
 class Player < Chingu::GameObject
-	def move_left; @x -= 5; end
-	def move_right; @x += 5; end
-	def move_up; @y -= 5; end
-	def move_down; @y += 3; end
+	def initialize(options = {})
+		super
+		@image = Gosu::Image["assets/player/player.png"]
+		@speed = 5
+		@width, @height = 32, 32
+	end
+
+	def move_left
+		if(@x > @speed + (@width/2))
+			@x -= @speed
+		end
+	end
+
+	def move_right
+		if(@x < 800 - @speed - (@width/2) )
+			@x += @speed
+		end
+	end
+
+	def move_up
+		if(@y - @speed - (@height/2) > 0)
+	 		@y -= @speed
+	 	end 
+	end
+
+	def move_down
+		if(@y + @speed + (@height/2) < 600)
+	 		@y += @speed
+	 	end
+	end
 
 	def fire
-		Bullet.create(:x => @x, :y => @y)
+		Bullet.create(:x => @x, :y => @y-32)
 	end
 end
 
@@ -29,30 +58,43 @@ class Bullet < Chingu::GameObject
 		super(options.merge(:image => Gosu::Image["assets/player/laser.png"]))
 	end
 	def update
-		@y -= 2
+		@y -= 5
+	end
+end
+
+class Introduction < Chingu::GameState
+	def initialize
+		super
+		fill(Color::Red)
+		@text = Chingu::Text.new("Welcome to ChinguRoids", :x => $window.WIDTH/4, :y => $window.HEIGHT/2, :font => "GeosansLight", :size => 30)
+		@text.x = $window.WIDTH/2 - @text.width/2
+
+		self.input = {:return => :next}
+	end
+	def next
+		push_game_state(Play)
+	end
+	def draw
+		@text.draw
 	end
 end
 
 class Play < Chingu::GameState
 	def initialize
 		super 
-		# OBJECTS
-		@player = Player.create(:x => 400, :y => 300, :image => Gosu::Image["assets/player/player.png"])
+		fill(Color::RED)
+		@player = Player.create(:x => 400, :y => 300)
 		@player.input = {:holding_left => :move_left, :holding_right => :move_right, :holding_up => :move_up, :holding_down => :move_down, :space => :fire}
 	end
 
-	def debug
-		push_game_state(Chingu::GameStates::Debug.new)
-	end
-
 	def draw
+		super
 		Gosu::Image["assets/window/background.png"].draw(0, 0, 0)
 	end
 
 	def update
 		super
 		Bullet.destroy_if {|bullet| bullet.outside_window?}
-		$window.caption = "ChinguRoids 0.01 FPS: #{$window.fps}"
 	end
 end
 
